bb7a2a577dc041954cad57ff68a1b8f047c575c2
Ryo Arima <ryo.arima@outlook.com>
2024-09-03 05:15:17 +0900

    🔎

--- tool/main.sh
+++ tool/main.sh
@@ -4,23 +4,29 @@ COMMAND="$1"
 ARGs="$@"
 PARAMs=(${ARGs// / })
 
+
 function build_server(){
-    go build -o bin/server cmd/server/main.go
+    go build -v -o bin/server cmd/server/main.go
+}
+
+function build_client_root(){
+    go build -v -o bin/mark1-ctl cmd/client/root/main.go
 }
 
 function build_client_anonymous(){
-    go build -o bin/anonymous-client cmd/client/anonymous/main.go
+    go build -v -o bin/anonymous-client cmd/client/anonymous/main.go
 }
 
 function build_client_app(){
-    go build -o bin/app-client cmd/client/app/main.go
+    go build -v -o bin/app-client cmd/client/app/main.go
 }
 
 function build_client_admin(){
-    go build -o bin/admin-client cmd/client/admin/main.go
+    go build -v -o bin/admin-client cmd/client/admin/main.go
 }
 
 function build_client(){
+    build_client_root
     build_client_anonymous
     build_client_app
     build_client_admin
@@ -44,15 +50,23 @@ function build-container-up(){
 }
 
 function build-deb(){
+    build
     VERSION=$(cat ./VERSION)
     DATETIME=$(date "+%a, %d %b %Y %H:%M:%S +0900" --date="2024-09-01 12:00:00")
     ARCH=$(uname -m)
     mkdir -p ./tool/deb/debian
-    cp -r ./bin ./tool/deb/debian
+    cp -r ./bin ./tool/deb
+    cp -r ./tool/systemd ./tool/deb
+    mkdir -p ./tool/deb/etc
+    cp etc/main.yaml.template ./tool/deb/etc/main.yaml
     eval "echo \"$(cat ./tool/deb/changelog.template)\"" > ./tool/deb/debian/changelog
     eval "echo \"$(cat ./tool/deb/control.template)\"" > ./tool/deb/debian/control
     eval "echo \"$(cat ./tool/deb/copyright.template)\"" > ./tool/deb/debian/copyright
-    cd ./tool/deb/ && dpkg-buildpackage -us -uc
+    eval "echo \"$(cat ./tool/deb/rules.template)\"" > ./tool/deb/debian/rules
+    cp ./tool/deb/mark1.install ./tool/deb/debian/mark1.install
+    chmod +x ./tool/deb/debian/rules
+
+    cd ./tool/deb/ && fakeroot dpkg-buildpackage -us -uc
 }
 
 $COMMAND
