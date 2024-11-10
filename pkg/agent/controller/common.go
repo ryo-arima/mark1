3115c83e553664c185c6b04426253c04bff558e4
Ryo Arima🌱 <118931098+ryo-arima@users.noreply.github.com>
2024-11-10 12:26:27 +0900

    🚋

--- pkg/agent/controller/common.go
+++ pkg/agent/controller/common.go
@@ -9,3 +9,19 @@ import (
 func (agentServer *config.AgentServer) Executer(ctx context.Context, request *auto.CommandRequest) (*auto.CommandReply, error) {
 	return &auto.CommandReply{Message: "Hello "}, nil
 }
+
+func (agentServer *config.AgentServer) RtcServer(ctx context.Context, request *auto.CommandRequest) (*auto.CommandReply, error) {
+	return &auto.CommandReply{Message: "Hello "}, nil
+}
+
+func (agentServer *config.AgentServer) RtcClient(ctx context.Context, request *auto.CommandRequest) (*auto.CommandReply, error) {
+	return &auto.CommandReply{Message: "Hello "}, nil
+}
+
+func (agentServer *config.AgentServer) DtnServer(ctx context.Context, request *auto.CommandRequest) (*auto.CommandReply, error) {
+	return &auto.CommandReply{Message: "Hello "}, nil
+}
+
+func (agentServer *config.AgentServer) DtnClient(ctx context.Context, request *auto.CommandRequest) (*auto.CommandReply, error) {
+	return &auto.CommandReply{Message: "Hello "}, nil
+}
--- pkg/entity/request/command.go
+++ pkg/entity/request/command.go
@@ -1,16 +1,36 @@
 package request
 
-type CommandRequest struct {
-	Command Command `json:"command"`
+type P2PCommandRequest struct {
+	Mode    string     `json:"mode"` //RTC or DTN
+	Command P2PCommand `json:"command"`
 }
 
-type Command struct {
-	AgentUUID string        `json:"agent_uuid"`
-	AgentName string        `json:"agent_name"`
-	Body      []CommandBody `json:"body"`
+type P2PCommand struct {
+	SrcAgentUUID string           `json:"src_agent_uuid"`
+	SrcAgentName string           `json:"src_agent_name"`
+	DstAgentUUID string           `json:"dst_agent_uuid"`
+	DstAgentName string           `json:"dst_agent_name"`
+	Body         []P2PCommandBody `json:"body"`
 }
 
-type CommandBody struct {
+type P2PCommandBody struct {
+	Environments string `json:"environments"`
+	Shell        string `json:"shell"`
+	Args         string `json:"args"`
+	Timeout      int    `json:"timeout"`
+}
+
+type SingleCommandRequest struct {
+	Command SingleCommand `json:"command"`
+}
+
+type SingleCommand struct {
+	AgentUUID string              `json:"agent_uuid"`
+	AgentName string              `json:"agent_name"`
+	Body      []SingleCommandBody `json:"body"`
+}
+
+type SingleCommandBody struct {
 	Environments string `json:"environments"`
 	Shell        string `json:"shell"`
 	Args         string `json:"args"`
